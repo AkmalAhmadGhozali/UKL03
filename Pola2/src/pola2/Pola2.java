632d9eb74919b7d1efa1cd5ff9325ac49e12a1cb
WINDOWS 10 <WINDOWS 10@LAPTOP-TQT7K6BC>
2021-01-14 19:57:01 +0700

    first commit

---
+++ Pola2/src/pola2/Pola2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pola2;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Pola2 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1; i<=4; i++){
+            for(j=1; j<=i; j++){
+                System.out.print("@");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
