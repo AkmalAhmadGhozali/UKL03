248e1acfc87f4c45b6e53a4203f73eff73f9c797
WINDOWS 10 <WINDOWS 10@LAPTOP-TQT7K6BC>
2021-07-30 09:22:29 +0700

    first commit

--- Pola2/src/pola2/Pola2.java
+++ Pola2/src/pola2/Pola2.java
@@ -14,7 +14,7 @@ public class Pola2 {
         int i,j;
         for(i=1; i<=4; i++){
             for(j=1; j<=i; j++){
-                System.out.print("@");
+                System.out.print("*");
             }
             System.out.println("");
         }
